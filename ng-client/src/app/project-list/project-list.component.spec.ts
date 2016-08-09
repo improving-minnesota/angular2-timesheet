3a00ac65f1c542d32bf759230d06194006142273
Michael Cacek <michael.cacek@objectpartners.com>
2016-08-08 23:41:51 -0500

    Add test for project list.

--- ng-client/src/app/project-list/project-list.component.spec.ts
+++ ng-client/src/app/project-list/project-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { By }           from '@angular/platform-browser';
+import { By } from '@angular/platform-browser';
 
 import {
   inject,
@@ -17,6 +17,8 @@ import { MdIconRegistry } from '@angular2-material/icon/icon';
 import { ProjectListComponent } from './project-list.component';
 
 describe('Component: ProjectList', () => {
+  let mdIconRegistry;
+  let mdIconRegistryProvider;
   let projectService;
   let projectServiceProvider;
   let router;
@@ -24,18 +26,16 @@ describe('Component: ProjectList', () => {
   let builder;
 
   beforeEach(() => {
-    let project = new Project({
-      _id: '1',
-      name: 'Project',
-      description: 'description'
-    });
+    mdIconRegistry = {
+      getDefaultFontSetClass: jasmine.createSpy('getDefaultFontSetClass')
+    };
+    mdIconRegistryProvider = {
+      provide: MdIconRegistry,
+      useFactory: () => mdIconRegistry
+    };
+
     projectService = {
       getProjects: jasmine.createSpy('getProjects')
-      // getProjects: () => {
-      //   return Observable.create((obs) => {
-      //     obs.next([project]);
-      //   });
-      // }
     };
     projectServiceProvider = {
       provide: ProjectService,
@@ -46,7 +46,7 @@ describe('Component: ProjectList', () => {
       navigateByUrl: jasmine.createSpy('navigateByUrl')
     };
     routerProvider = {
-      provider: Router,
+      provide: Router,
       useFactory: () => router
     };
   });
@@ -54,21 +54,66 @@ describe('Component: ProjectList', () => {
   let deps = [TestComponentBuilder];
   beforeEach(inject(deps, fakeAsync((tcb: TestComponentBuilder) => {
       builder = tcb.overrideProviders(ProjectListComponent, [
+          mdIconRegistryProvider,
           projectServiceProvider,
           routerProvider
       ]);
   })));
 
-  // it('should create an instance', (done) => {
-  //   return builder
-  //     .createAsync(ProjectListComponent)
-  //     .then((fixture: ComponentFixture<ProjectListComponent>) => {
-  //       fixture.detectChanges();
-  //
-  //       // let spans = fixture.debugElement.queryAll(By.css('span'));
-  //       // expect(spans[4].nativeElement.innerHTML).toEqual('John Doe');
-  //
-  //       done();
-  //     });
-  // });
+  it('should list all provided projects', (done) => {
+    let project = new Project({
+      _id: '1',
+      name: 'Project',
+      description: 'description'
+    });
+
+    projectService.getProjects.and.callFake(() => {
+      return Observable.create((obs) => {
+        obs.next([project]);
+      });
+    });
+
+    return builder
+      .createAsync(ProjectListComponent)
+      .then((fixture: ComponentFixture<ProjectListComponent>) => {
+        fixture.detectChanges();
+
+        let listItems = fixture.debugElement.queryAll(By.css('md-list-item'));
+
+        expect(listItems.length).toBe(2);
+        expect(listItems[1].query(By.css('h3')).nativeElement.innerHTML).toBe('Project');
+        expect(listItems[1].query(By.css('p')).nativeElement.innerHTML).toBe('description');
+
+        done();
+      });
+  });
+
+  it('should respond to user click and navigate to proejct creation', (done) => {
+    let project = new Project({
+      _id: '1',
+      name: 'Project',
+      description: 'description'
+    });
+
+    projectService.getProjects.and.callFake(() => {
+      return Observable.create((obs) => {
+        obs.next([project]);
+      });
+    });
+
+    return builder
+      .createAsync(ProjectListComponent)
+      .then((fixture: ComponentFixture<ProjectListComponent>) => {
+        fixture.detectChanges();
+
+        let addBtn = fixture.debugElement.query(By.css('button'));
+
+        addBtn.nativeElement.click();
+
+        expect(router.navigateByUrl).toHaveBeenCalledTimes(1);
+        expect(router.navigateByUrl).toHaveBeenCalledWith('/home/projects/new');
+
+        done();
+      });
+  });
 });
